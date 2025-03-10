18747515cccf3ec41c137ff8a51a2a42c1563a17
mandardahe29 <75142994+mandardahe29@users.noreply.github.com>
2025-03-10 15:00:15 +0530

    Add files via upload

---
+++ Stream/main_app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from impact_test import impact_test_page
+from cft_test import cft_test_page
+from tft_test import tft_test_page
+from rdt_test import rdt_test_page
+from axial_test import axial_test_page
+
+def main():
+    st.title("Test Selection")
+
+    test_options = [
+        "Impact Test",
+        "CFT Test",
+        "TFT Test",
+        "RDT Test",
+        "Axial Test"
+    ]
+    choice = st.selectbox("Select a test:", test_options)
+
+    if choice == "Impact Test":
+        impact_test_page()
+    elif choice == "CFT Test":
+        cft_test_page()
+    elif choice == "TFT Test":
+        tft_test_page()
+    elif choice == "RDT Test":
+        rdt_test_page()
+    elif choice == "Axial Test":
+        axial_test_page()
+
+if __name__ == '__main__':
+    main()
